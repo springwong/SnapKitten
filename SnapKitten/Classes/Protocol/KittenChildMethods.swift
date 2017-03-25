d142053863e5a161f8636525cea87fdc7fd40758
Spring Wong <spring@Springs-MacBook-Air.local>
2017-03-25 20:57:43 +0800

    Added new constructor and status change method to more easy to use

--- SnapKitten/Classes/Protocol/KittenChildMethods.swift
+++ SnapKitten/Classes/Protocol/KittenChildMethods.swift
@@ -23,8 +23,15 @@ public protocol KittenChildMethods : KittenChild, KittenBuild{
     @discardableResult func height(_ value : Int?, _ condition : KittenSign) -> KittenChildMethods
     //set width, height at the same time
     @discardableResult func size(_ value : Int?, _ condition : KittenSign) -> KittenChildMethods
+    @discardableResult func width(_ value : Int?) -> KittenChildMethods
+    @discardableResult func height(_ value : Int?) -> KittenChildMethods
+    @discardableResult func size(_ value : Int?) -> KittenChildMethods
     //set perpendicular alignment, .parent, .start, .center, .end . start is top/left , end is bottom/right
     @discardableResult func align(_ alignment : KittenAlignment) -> KittenChildMethods
+    @discardableResult func alignSideStart() -> KittenChildMethods
+    @discardableResult func alignSideCenter() -> KittenChildMethods
+    @discardableResult func alignSideEnd() -> KittenChildMethods
+    @discardableResult func alignSideParent() -> KittenChildMethods
     //check if item is added to kitten, if false, child will be ignored in the build()
     @discardableResult func condition(_ condition : @escaping KittenInsertCondition) -> KittenChildMethods
     //add a fill parent space, which always compress itself if no enough space in other items
@@ -33,6 +40,9 @@ public protocol KittenChildMethods : KittenChild, KittenBuild{
     @discardableResult func fitSize() -> KittenChildMethods
     //the importanence of child, default .high, child with .medium / .low with higher prioity to be compressed by other child
     @discardableResult func priority(_ priority : KittenPriority) -> KittenChildMethods
+    @discardableResult func importanceHigh() -> KittenChildMethods
+    @discardableResult func importanceMedium() -> KittenChildMethods
+    @discardableResult func importanceLow() -> KittenChildMethods
     //used only in weight mode, child separate screen with it's weight, all childs weight 0 is not allowed in ios side
     @discardableResult func weight(_ value : Float) -> KittenChildMethods
 }
